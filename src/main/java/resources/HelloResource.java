218ee1b46d2852b27b3f5323841920e5f583bab2
Fabiano Bandeira <lfabianosb@gmail.com>
2017-04-09 11:54:56 -0300

    First commit

---
+++ src/main/java/resources/HelloResource.java
@@ -0,0 +1,16 @@
+package resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello")
+public class HelloResource {
+
+	@GET
+	@Produces("text/plain")
+	public String handleGreeting() {
+		return "Funcionou asdfasdfasdfasdf!!!!!!!!!!!!";
+	}
+
+}
